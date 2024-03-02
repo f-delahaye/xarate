6a1e63e324603c38889fe28aa05b900cb5004b4c
f-delahaye <fd.developer@gmail.com>
2024-03-02 20:50:00 +0100

    Make pluginVerifier happy

--- xarate-idea/src/main/java/io/xarate/idea/run/XarateRunConfigurationType.java
+++ xarate-idea/src/main/java/io/xarate/idea/run/XarateRunConfigurationType.java
@@ -3,13 +3,14 @@ package io.xarate.idea.run;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.execution.configurations.SimpleConfigurationType;
 import com.intellij.icons.AllIcons;
+import com.intellij.ide.ui.ProductIcons;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.NotNullLazyValue;
 import org.jetbrains.annotations.NotNull;
 
 public class XarateRunConfigurationType extends SimpleConfigurationType {
     protected XarateRunConfigurationType() {
-        super("XarateRunConfiguration", "Xarate", "Xarate Run Configuration", NotNullLazyValue.createValue(() -> AllIcons.Icon));
+        super("XarateRunConfiguration", "Xarate", "Xarate Run Configuration", NotNullLazyValue.createValue(() -> ProductIcons.getInstance().getProductIcon()));
     }
 
     @Override
